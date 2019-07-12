09d0968bcaf375b80b19ba000f582135404974d6
Courtney Solari <csolari@ufl.edu>
2019-07-11 22:59:55 -0500

    Add declaration for loopThrough function

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@
 const myBoxes = document.getElementsByClassName("block");
 
 // The Functionality
+function loopThrough() {
 
+	const changeColor = () => this.classList.toggle("new-color");
+
+	for (let i = 0; i < this.textContent.length * 2; i++) {
+		setTimeout(changeColor, i * 1000);
+	}
+}
 
 // The Event Listener
